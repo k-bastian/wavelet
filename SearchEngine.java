8457992085a7747c03227ca9dabf8729ae919acd
k-bastian <ksebastian@ucsd.edu>
2022-10-13 18:21:26 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,48 +1,51 @@
 import java.io.IOException;
 import java.net.URI;
-import java.util.List;
 import java.util.ArrayList;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    int num = 0;
     String word = "";
     String key = "";
-    List<String> list = new ArrayList<String>();
-    List<String> searched = new ArrayList<String>();
-    
+    ArrayList<String> list = new ArrayList<String>();
+    ArrayList<String> searched = new ArrayList<String>();
 
     public String handleRequest(URI url) {
+        System.out.println("Path: " + url.getPath());
+
         if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
-        } else {
-            System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("s")) {
-                    word = parameters[1];
-                    list.add(word);
-                    return String.format("Word is %s!", parameters[1]);
-                }
-            }
-            else if (url.getPath().contains("/search")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("s")) {
-                    key = parameters[1];
-                    for(String s : list) {
-                        if(s.contains(key)) {
-                            searched.add(s);                            
-                        }
-                    }
-                    return String.format("Found String! %s", searched);
-                }
-            }
-            return "404 Not Found!";
+            return String.format("Search Engine");
         }
+        else if (url.getPath().contains("/add")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                word = parameters[1];
+                list.add(word);
+                return String.format("Word added: %s!", word);
+            }
+        }
+        else if (url.getPath().contains("/search")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                key = parameters[1];
+                for(String s : list) {
+                    if(s.contains(key)) {
+                        searched.add(s);
+                    }
+                }          
+                
+                ArrayList<String> noDuplicates = new ArrayList<String>();
+
+                for(String s: searched) {
+                    if (!noDuplicates.contains(s)) {
+                        noDuplicates.add(s);
+                    }
+                }
+
+                return String.format("Found %s in %s", key, noDuplicates);
+            }
+        }
+        return "404 Not Found!";
     }
 }
 
